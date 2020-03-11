c0d9a7f2e48683cb8feed820f60062a16ee34f49
rebixpl <33410494+rebixpl@users.noreply.github.com>
2020-03-11 17:10:45 +0100

    Create mi_card_flutter_v_1_5.dart

---
+++ flutter/mi_card_flutter/mi_card_flutter_v_1_5.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              // CircleAvatar create a circle that represents a user ( you can set profile image, etc )
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/myFlyFace.png"),
+                // backgroundColor: Colors.red, // You can set background color as well as image
+              ),
+              Text(
+                "Katsuhira Agata",
+                style: TextStyle(
+                  fontFamily: "Pacifico",
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: "Source Sans Pro",
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              // PHONE CONTAINER
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "+43 696 696 420",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "Source Sans Pro",
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // EMAIL CONTAINER
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "rebixcraft@gmail.com",
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: "Source Sans Pro",
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
